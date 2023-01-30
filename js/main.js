3ef8d4e959c1f8ef4ca8193c7dad9e1bc3869ec7
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-01-31 00:45:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-let navBtn = document.querySelector(".navgation .bars");
+let navBtn = document.querySelector(".navgation .menu-container");
 let navContent = document.querySelector(".nav");
 navBtn.onclick = () => {
     navBtn.classList.toggle("open");
     navContent.classList.toggle("open");
 };
-
-// Start get Api From Website

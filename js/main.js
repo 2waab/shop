145e5024be6e0d0cb3ac3c4dced8ba3359ff55f0
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-02-01 23:29:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let headOfPage = document.querySelector(".header");
 let navContent = document.querySelector(".nav");
 let contentOfBoxesOfTop = document.querySelector(".top-rate .responsive-box");
 let contentOfBoxesOfClothing = document.querySelector(".clothing .responsive-box");
+let contentOfBoxesOfForWomen = document.querySelector(".for-women .responsive-box");
 navBtn.onclick = () => {
     navBtn.classList.toggle("open");
     navContent.classList.toggle("open");
@@ -13,6 +14,7 @@ fetch ("https://fakestoreapi.com/products")
     .then((res) => res.json())
     .then((data) => {
         for (let i = 0; i < data.length; i++) {
+            console.log(data[i].category)
             if (data[i].rating.rate > 4.5) {
                 let box = document.createElement("div");
                 box.className = "box-of-prodact";
@@ -25,7 +27,10 @@ fetch ("https://fakestoreapi.com/products")
                             <h5>${data[i].title}</h5>
                             <span><i class="fa-solid fa-star"></i> ${data[i].rating.rate}</span>
                         </div>
-                        <div class="catogry">${data[i].category}</div>
+                        <div class="cat">
+                            <div class="catogry">${data[i].category}</div>
+                            <div class="catogry">top rate</div>
+                        </div>
                         <div class="foot">
                             <p>$${data[i].price}</p>
                             <button><i class="fa-solid fa-shopping-bag"></i></button>
@@ -54,6 +59,27 @@ fetch ("https://fakestoreapi.com/products")
                     </div>
                 `;
                 contentOfBoxesOfClothing.appendChild(box);
+            };
+            if (data[i].category === "women's clothing") {
+                let box = document.createElement("div");
+                box.className = "box-of-prodact";
+                box.innerHTML = `
+                    <div class="img">
+                    <img src="${data[i].image}" alt="" />
+                    </div>
+                    <div class="text">
+                        <div class="head">
+                            <h5>${data[i].title}</h5>
+                            <span><i class="fa-solid fa-star"></i> ${data[i].rating.rate}</span>
+                        </div>
+                        <div class="catogry">${data[i].category}</div>
+                        <div class="foot">
+                            <p>$${data[i].price}</p>
+                            <button><i class="fa-solid fa-shopping-bag"></i></button>
+                        </div>
+                    </div>
+                `;
+                contentOfBoxesOfForWomen.appendChild(box);
             };
         };
     });
